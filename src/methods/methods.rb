e3d0c7fedd451968805d218a7f2fbb01d97fbdce
isaaccavallaro <isaaccavallaro@gmail.com>
2021-05-18 17:35:49 +1000

    updating classes

--- src/methods/methods.rb
+++ src/methods/methods.rb
@@ -1,5 +1,22 @@
-#required gems
+#Import gems
 require "artii"
 require "tty-prompt"
 require "colorize"
+require "tty-prompt"
 
+# Import Classes
+require_relative "../classes/Users.rb"
+require_relative "../classes/ChordProgression.rb"
+
+#Initialize TTY prompt
+prompt = TTY::Prompt.new
+
+#Clear terminal to ready app
+system('clear')
+
+# Get the users name
+
+    prompt.ask("What is your name?", default: ["Musician"])
+    name = gets.chomp
+    
+puts name
