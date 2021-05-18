02684bcb5afc70b0c5ed04e6bac47d4604741e56
isaaccavallaro <isaaccavallaro@gmail.com>
2021-05-18 13:37:56 +1000

    installing colorize and artii

---
+++ src/classes/practice_session.rb
@@ -0,0 +1,37 @@
+# class PracticeSession
+
+#     attr_accessor :name, :key, :chords
+
+#     def initialize(name, key, chords)
+#       @name = name
+#       @key = key
+#       @chords = chords
+#     end
+
+#     def to_s
+#         return "This progression #{@progression} in the key of #{@key}, translates to these chords #{@chords}"
+#     end
+
+#     def logged_practice(name, key, chords)
+
+#     end
+
+
+# end 
+
+# name = []
+# key = []
+# chords = []
+
+# level_one = [['I','IV','I','I'],['VI','VI','I','I'],['II','V','I','V']]
+# level_two = [['I','IV','I','I'],['VI','VI','I','I'],['II','V','I','V']]
+# level_three = [['I','V','I','I'],['VI','VI','I','VI'],['ii','V','I','V']]
+
+
+# levels_array = [level_one, level_two, level_three]
+
+# puts level_two[0].to_s
+# puts level_two[1].to_s
+# puts level_two[2].to_s
+
+
